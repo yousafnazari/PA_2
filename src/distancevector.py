e48123a5fa0b8084746c177c8ffd4bdd90a88c58
jaydensahl <56840991+jaydensahl@users.noreply.github.com>
2024-03-21 19:26:43 -0700

    read file

--- src/distancevector.py
+++ src/distancevector.py
@@ -7,25 +7,27 @@ class Node:
     def dvr_update_neighbours(self,change):
         self.sendnameplaceholder = []
 
+def apply_topology_to_nodes(data)
 
 def distancevector(topology, message, changes):
-    #function definition
-    print(f"Hello {topology}, {message}, {changes}")
+    topology_data = get_data(topology)
+    #message_data = get_data(message)
+    changes_data = get_data(changes)
 
 
 
 
 
-
-
-def get_topology(filename):
+def get_data(filename):
     nodes = {}
+    data = []
     with open(filename,'r') as file:
-        topologydata = file.read()
-        print(topologydata)
-
+        for line in file:
+            data.append([int(num) for num in line.split()])
+        print(data)
+    return data
 
 
 
 if __name__ == '__main__':
-    distancevector('topologyFile.txt','messageFile','changesFile')
+    distancevector('topologyFile.txt','messageFile.txt','changesFile.txt')
