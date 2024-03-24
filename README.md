d5a7b7743ec820654f5e0396be812381cc7cb00f
jaydensahl <56840991+jaydensahl@users.noreply.github.com>
2024-03-23 21:38:25 -0700

    added path cost

---
+++ README.md
@@ -0,0 +1,3 @@
+to run in bash script:
+dvr.sh topologyFile.txt messageFile.txt changesFile.txt [ouputFile.txt]
+[] means optional 
--- src/distancevector.py
+++ src/distancevector.py
@@ -19,7 +19,6 @@ class Network:
         while updated:
             updated = False
 
-            #do for each node
             for node_id, node in self.nodes.items():
                 #do for each neighbour of node
                 for neighbour_id, cost in node.neighbours.items():
@@ -30,7 +29,6 @@ class Network:
                         if destination not in node.forwarding_table or cost + path_cost < node.forwarding_table[destination][1]:
                             node.update_forwarding_table(destination,neighbour_id,cost+path_cost)
                             updated = True
-
     
 class Node:
     def __init__(self):
@@ -74,13 +72,12 @@ def apply_topology_to_nodes(data, network):
             node_next.update_forwarding_table(neighbour_id,neighbour_id,0)
         else:
             node.remove_neighbour(neighbour_id)
-            neighbour_id.remove_neighbour(node_id)
+            node_next.remove_neighbour(node_id)
 
-def generate_forwarding_table(node,outputFile,file):
+def generate_forwarding_table(node,file):
     
-    #with open(outputFile,'a') as file:
-        for destination, (next_hop, path_cost) in sorted(node.forwarding_table.items()):
-            file.write(f"{destination} {next_hop} {path_cost}\n")
+    for destination, (next_hop, path_cost) in sorted(node.forwarding_table.items()):
+        file.write(f"{destination} {next_hop} {path_cost}\n")
     
 
 def send_message(source, destination, path_cost, path, message, file):
@@ -98,8 +95,8 @@ def read_message(messageFile):
     with open(messageFile,'r') as file:
         for line in file:
             piece = line.split(maxsplit=2)
-            from_node_id = str(piece[0])
-            to_node_id = str(piece[1])
+            from_node_id = int(piece[0])
+            to_node_id = int(piece[1])
             message = piece[2].strip()
             messages.append((from_node_id,to_node_id,message))
     return messages
@@ -113,16 +110,43 @@ def get_data(filename):
         #print(data)
     return data
 
-def output_data(output, messages, network):
-    with open(output,'w') as file:
+def output_data(file, messages, network):
+    
             # generate fowarding tables
         for node_id, node in sorted(network.nodes.items()):
             file.write(f"node {node_id} forwarding table\n")
-            generate_forwarding_table(node,output,file)
+            generate_forwarding_table(node,file)
         for from_id, to_id, message in messages:
-            send_message(from_id,to_id,7,[2, 1],message,file)
+            #find message distance
+            node = network.nodes.get(from_id,Node())
+            next_hop, path_to_destination = node.forwarding_table[to_id]
+            #get hops
+            hops = []
+            hops.append(from_id)
+            '''
+            traversed = True
+            while traversed:
+                node = network.nodes.get(next_hop,Node())
+                hops.append(next_hop)
+                next_hop, path_cost = node.forwarding_table[to_id]
+                if path_cost == 0:
+                    traversed = False
+             '''       
+            send_message(from_id,to_id,path_to_destination,hops,message,file)
         
+def apply_change(network,change):
 
+        node1_id = change[0]
+        node2_id = change[1]
+        node1 = network.nodes.get(node1_id, Node())
+        node2 = network.nodes.get(node2_id, Node())
+        new_cost = change[2]
+        if new_cost != -999:
+            node1.update_forwarding_table(node2_id,node2_id,new_cost)
+            node2.update_forwarding_table(node1_id,node1_id,new_cost)
+        else:
+            node1.remove_neighbour(node2_id)
+            node2.remove_neighbour(node1_id)
 
 def distancevector(topology, message, changes, network, output='outputFile.txt'):
 
@@ -131,15 +155,22 @@ def distancevector(topology, message, changes, network, output='outputFile.txt')
     apply_topology_to_nodes(topology_data, network)
     #read messages to send
     messages = read_message(message)
-    network.converge()
-    '''
-    ADD FORWARDING TABLE CONGERENCE ALGO HERE
-    '''
-    #output forwarding tables and sent messages
-    output_data(output, messages, network)
-
-
     changes_data = get_data(changes)
+    with open(output,'w') as file:
+        #output_data(file,messages,network)
+        for change in changes_data:
+            apply_change(network,change)
+            network.converge()
+            output_data(file, messages, network)
+
+
+    ###########
+    # ADD change file implementation, reconverge, reoutput
+    ###########
+    #output forwarding tables and sent messages
+    
+
+
 
 
 
@@ -154,4 +185,6 @@ if __name__ == '__main__':
         distancevector(top,msg,chg,network,out)
     else: 
         distancevector(top,msg,chg,network)
-    print(f"{top},{msg},{chg},{out}")
+    #print(f"{top},{msg},{chg},{out}")
+    
+    #distancevector('topologyFile.txt','messageFile.txt','changesFile.txt',network)
